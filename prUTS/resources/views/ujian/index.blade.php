2c79ca2dfc8569b7a503b2964a6ce622eda4fe18
divaaldita <divaaldita12@gmail.com>
2024-05-07 17:08:11 +0700

    jhvyv

---
+++ prUTS/resources/views/ujian/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kelompok 7</title>
+</head>
+<body>
+    <h1>Kelompok 7</h1>
+    <p>Nama: [Nama anda]</p>
+    <p>NIM: [NIM]</p>
+    <p>Major: [Major]</p>
+    <p>Semester: [Semester]</p>
+
+    <script>
+        alert("{{session('kunci')}}");
+    </script>
+</body>
+</html>
---
+++ prUTS/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/uts', [UTSController::class, 'index'])->name('index');
+Route::get('/uts/create', [UTSController::class, 'create'])->name('create');
+Route::post('/uts', [UTSController::class, 'store'])->name('store');
